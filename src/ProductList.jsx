d0e9103aa5edc1235df7aa8cddd7d61b074d000a
KevinWang92 <kevin.wang@paradyme.us>
2024-07-09 13:14:47 -0400

    steps 1-7

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,17 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 function ProductList() {
-  
+
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+        ...prevState,
+        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+    }));
+    };
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -252,6 +262,32 @@ function ProductList() {
 
         <div className="product-grid">
 
+            const items = [];
+            {plantsarray.foreach(item => (
+                <>
+                
+               
+                {plantsArray.map((category, index) => (
+                    <div key={index}>
+                        <h1><div>{category.category}</div></h1>
+                        <div className="product-list">
+                        {category.plants.map((plant, plantIndex) => (
+                            <div className="product-card" key={plantIndex}>
+                            <img className="product-image" src={plant.image} alt={plant.name} />
+                            <div className="product-title">{plant.name}</div>
+                    /*Similarly like the above plant.name show other details like description and cost*/
+                            <button  onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            </div>
+                        ))}
+                        </div>
+                    </div>
+                ))}
+                    
+  
+
+                </>
+                
+            ))}
 
         </div>
 
